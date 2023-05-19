7b35a20684144c1a66a09734f87f1665d1bae6ce
Your Name <you@example.com>
2023-05-19 06:10:06 +0530

    Sending a Transaction from a Website

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { ethers } from "./ethers-5.2.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
 
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
 connectButton.onclick = connect;
 fundButton.onclick = fund;
 
-console.log(ethers);
+// console.log(ethers);
 
 async function connect() {
     // checks whether we have metamask in browser using window.ethereum
@@ -28,8 +29,8 @@ async function connect() {
     }
 }
 
-async function fund(ethAmount) {
-    console.log("Fund");
+async function fund() {
+    const ethAmount = "7";
     if (typeof window.ethereum !== "undefined") {
         /*
         To send a transactions, we need 
@@ -38,5 +39,27 @@ async function fund(ethAmount) {
         - contract that we are interacting with
             - ^ ABI & Address
          */
+
+        // Web3Provider is an object in ethers that allows us to basically wrap around stuff like Metamask
+        //  Web3Provider is really similar to that JSON RPC provider, which we use before, which is where we put in exactly that endpoint, our alchemy endpoint, or when we're working with Metamask, whatever endpoint that we have in our network section
+        //  Web3Provider takes that HTTP endpoint and automatically sticks it in ethers for us.
+        // So this line of code basically looks at our Metamask and goes to the HTTP endpoint inside the Metamask. That's going to be what we're going to use as our provider here.
+        const provider = new ethers.providers.Web3Provider(
+            window.ethereum,
+            "any"
+        );
+
+        // returns whichever wallet connected to provider
+        const signer = provider.getSigner();
+
+        // Getting Contract
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            });
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 }
